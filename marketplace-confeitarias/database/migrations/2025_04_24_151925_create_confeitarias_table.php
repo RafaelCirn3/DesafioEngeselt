bdfe9f27bf54685fce0c5d535c66fe60ea90028d
rafinha <125037856+RafaelCirn3@users.noreply.github.com>
2025-04-24 12:22:31 -0300

    feat- arrow_up: adição do projeto + configuração do banco de dados e migrações

---
+++ marketplace-confeitarias/database/migrations/2025_04_24_151925_create_confeitarias_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confeitarias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->string('cep')->nullable();
+            $table->string('rua')->nullable();
+            $table->string('numero')->nullable();
+            $table->string('bairro')->nullable();
+            $table->string('cidade')->nullable();
+            $table->string('estado')->nullable();
+            $table->string('telefone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('confeitarias');
+    }
+};
